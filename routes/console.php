1923db9dceeb1b14d43b405741c4a7e988ea8d1a
Phil Stephens <phils@hey.com>
2024-08-24 09:26:23 +1000

    Artisan command to sync manually added FIT files

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,26 @@
 <?php
 
+use App\Models\Activity;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('sync-fit', function () {
+    $files = Storage::disk('public')->files('activities');
+
+    collect($files)->each(function ($file) {
+        if (! $exists = Activity::where('file', $file)->first()) {
+            Activity::create([
+                'file' => $file,
+            ]);
+        }
+    });
+});
 
 Schedule::command('backup:clean')->daily()->at('01:00');
 Schedule::command('backup:run')->daily()->at('01:30');
